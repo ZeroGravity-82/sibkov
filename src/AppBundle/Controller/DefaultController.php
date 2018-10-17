23c7696f9c5408e556e0210a2c236366a8ce10a3
Nikolay Ryabkov <nikolay.ryabkov@sibers.com>
2018-10-17 12:28:29 +0700

    add controllers

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -3,7 +3,6 @@
 namespace AppBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class DefaultController extends Controller
@@ -11,11 +10,34 @@ class DefaultController extends Controller
     /**
      * @Route("/", name="homepage")
      */
-    public function indexAction(Request $request)
+    public function indexAction()
     {
-        // replace this example code with whatever you need
         return $this->render('default/index.html.twig', [
             'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
         ]);
+    }
+
+    /**
+     * @Route("/nasha-produkciya")
+     */
+    public function ourProductsAction()
+    {
+
+    }
+
+    /**
+     * @Route("/kak-zakazat")
+     */
+    public function howToOrderAction()
+    {
+
+    }
+
+    /**
+     * @Route("/kontakty")
+     */
+    public function contactsAction()
+    {
+
     }
 }
