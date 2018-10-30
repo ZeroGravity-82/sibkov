5db9b709674e4b1ff7ce60b7a5c7e73cbe947ac0
Nikolay Ryabkov <nikolay.ryabkov@sibers.com>
2018-10-30 12:38:47 +0700

    fix route prefix for default controller

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -8,6 +8,8 @@ use Symfony\Component\Routing\Annotation\Route;
 /**
  * Class DefaultController
  * @package AppBundle\Controller
+ *
+ * @Route("/")
  */
 class DefaultController extends Controller
 {
