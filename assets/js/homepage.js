9ab6e80c365fb6ab96293417b0e1b6b5bf0dae4f
Nikolay Ryabkov <nikolay.ryabkov@sibers.com>
2018-11-22 09:12:42 +0700

    fix slider dots

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -2,17 +2,21 @@
 
 require('slick-carousel/slick/slick.css');
 require('slick-carousel/slick/slick-theme.css');
+require('../css/slick-slider.css');
 
 const $ = require('jquery');
 require('slick-carousel');
 
 $(document).ready(function () {
     $('.js-slider').slick({
-        infinite: true,
+        arrows: false,
         autoplay: true,
         autoplaySpeed: 5000,
-        speed: 750,
-        fade: true,
         cssEase: 'linear',
+        dots: true,
+        fade: true,
+        infinite: true,
+        pauseOnHover: true,
+        speed: 750,
     });
 });
