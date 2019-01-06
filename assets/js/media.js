1966741d128d2ce0ea686f848212e62203daceee
Nikolay Ryabkov <nikolay.ryabkov@sibers.com>
2019-01-06 18:50:15 +0700

    refactor lightgallery in order to use with webpack encore

--- assets/js/media.js
+++ assets/js/media.js
@@ -1,3 +1,21 @@
 'use strict';
 
+require('lightgallery/dist/css/lightgallery.css');
 require('../css/media.css');
+
+const $ = require('jquery');
+require('lightgallery');
+require('lg-video');
+
+$(document).ready(function() {
+    $('#video-gallery').lightGallery({
+        selector: '.album-item',
+        getCaptionFromTitleOrAlt: false,
+        youtubePlayerParams: {
+            modestbranding: 1,
+            showinfo: 0,
+            rel: 0,
+            controls: 1
+        }
+    });
+});
