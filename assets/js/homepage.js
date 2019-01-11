3fbd2fe8e09f8e74b302b7562dca96986c4db9a5
Nikolay Ryabkov <nikolay.ryabkov@sibers.com>
2019-01-11 11:42:36 +0700

    fix styling for the slider on the homepage

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -9,10 +9,10 @@ require('slick-carousel');
 
 $(document).ready(function () {
     $('.js-slider').slick({
-        arrows: false,
         autoplay: true,
         autoplaySpeed: 5000,
         cssEase: 'linear',
+        arrows: true,
         dots: true,
         fade: true,
         infinite: true,
