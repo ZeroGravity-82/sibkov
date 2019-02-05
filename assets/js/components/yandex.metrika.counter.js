9d4de7ab25b9d71192296645c42d2151b8f8a252
Nikolay Ryabkov <nikolay.ryabkov@sibers.com>
2019-02-05 19:29:40 +0700

    fix counter for yandex.metrika

--- assets/js/components/yandex.metrika.counter.js
+++ assets/js/components/yandex.metrika.counter.js
@@ -1,25 +1,17 @@
 'use strict';
 
-(function (d, w, c) {
-	(w[c] = w[c] || []).push(function() {
-		try {
-			w.yaCounter21231592 = new Ya.Metrika({
-				id:21231592,
-				clickmap:true,
-				trackLinks:true,
-				accurateTrackBounce:true
-			});
-		} catch(e) { }
-	});
+(function (m, e, t, r, i, k, a) {
+    m[i] = m[i] || function () {
+        (m[i].a = m[i].a || []).push(arguments)
+    };
+    m[i].l = 1 * new Date();
+    k = e.createElement(t), a = e.getElementsByTagName(t)[0], k.async = 1, k.src = r, a.parentNode.insertBefore(k, a)
+})
+(window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
 
-	var n = d.getElementsByTagName("script")[0],
-		s = d.createElement("script"),
-		f = function () { n.parentNode.insertBefore(s, n); };
-	s.type = "text/javascript";
-	s.async = true;
-	s.src = (d.location.protocol == "https:" ? "https:" : "http:") + "//mc.yandex.ru/metrika/watch.js";
-
-	if (w.opera == "[object Opera]") {
-		d.addEventListener("DOMContentLoaded", f, false);
-	} else { f(); }
-})(document, window, "yandex_metrika_callbacks");
+ym(52238566, "init", {
+    id: 52238566,
+    clickmap: true,
+    trackLinks: true,
+    accurateTrackBounce: true
+});
