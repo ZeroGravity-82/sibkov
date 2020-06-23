a8264721ce4252c3b45fd92b004b06f7937c98f3
Nikolay Ryabkov <nikolay.ryabkov@sibers.com>
2020-06-24 00:35:28 +0700

    cut unnecessary comments from webpack-encore config file

--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,13 @@
 var Encore = require('@symfony/webpack-encore');
 
 Encore
-// directory where compiled assets will be stored
     .setOutputPath('web/build/')
-    // public path used by the web server to access the output path
     .setPublicPath('/build')
     .cleanupOutputBeforeBuild()
     .copyFiles({
         from: './assets/images/static',
         to: 'images/static/[path][name].[hash:8].[ext]'
     })
-    // only needed for CDN's or sub-directory deploy
-    //.setManifestKeyPrefix('build/')
-
-    /*
-     * ENTRY CONFIG
-     *
-     * Add 1 entry for each "page" of your app
-     * (including one that's included on every page - e.g. "app")
-     *
-     * Each entry will result in one JavaScript file (e.g. app.js)
-     * and one CSS file (e.g. app.css) if you JavaScript imports CSS.
-     */
     .addEntry('layout',   './assets/js/layout.js')
     .addEntry('homepage', './assets/js/homepage.js')
     .addEntry('product',  './assets/js/product.js')
